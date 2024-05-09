9190a057d1cef23260fcc97e7413b2f45092e1e7
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-05-09 20:10:59 +0300

    add social auth mutation to auth api

--- client/redux/features/auth/authApi.ts
+++ client/redux/features/auth/authApi.ts
@@ -72,7 +72,28 @@ export const authApi = apiSlice.injectEndpoints({
         }
       },
     }),
+    socialAuth: builder.mutation({
+      query: ({ email, name, avatar }) => ({
+        url: "users/social-auth",
+        method: "POST",
+        body: { email, name, avatar },
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(args, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error: any) {
+          console.log("activation", error.message);
+        }
+      },
+    }),
   }),
 });
 
-export const { useRegisterMutation, useActivationMutation, useLoginMutation } = authApi;
+export const { useRegisterMutation, useActivationMutation, useLoginMutation, useSocialAuthMutation } = authApi;
