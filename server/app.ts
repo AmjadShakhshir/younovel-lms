21813c762e71aa2615a5e150e3eefafcf16d1d65
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-05-10 19:35:00 +0300

    edit cors allowed websites

--- server/app.ts
+++ server/app.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://younovel-lms.vercel.app"],
     credentials: true,
   })
 );
