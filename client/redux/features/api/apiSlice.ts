af86d93fe457a7137c1c09c6a37158cc9f5b7893
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-05-14 20:45:31 +0300

    add jest unit testing

--- client/redux/features/api/apiSlice.ts
+++ client/redux/features/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { userLoggedIn } from "../auth/authSlice";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_SERVER_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PRIVATE_SERVER_URL }),
   endpoints: (builder) => ({
     refreshToken: builder.query({
       query: () => ({
