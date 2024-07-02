81cd17df49af84aee4830023a14ae53ab75c855b
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-07-02 05:52:56 +0300

    edit server file

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,8 @@ import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./utils/db";
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = Number(process.env.PORT) || 8000;
+const HOST = "0.0.0.0";
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -13,7 +14,7 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
   connectDB();
 });
